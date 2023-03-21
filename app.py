a93bcffe288daa02cdf58f10cb8e2eee2bd7404d
Aleystre Vocleine <40627577+AleystreV@users.noreply.github.com>
2023-03-21 18:31:26 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,44 +1,12 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from prediction import xgb_predict, lr_predict
+from prediction import xgb_predict, lr_predict, xgb_suicide, lr_suicide
 import nltk
 from nltk import sent_tokenize
 nltk.download('punkt');
 
 #st.write(st.__version__)
-
-st.markdown("""
-<style>
-    [data-testid=stSidebar] {
-        background-color: #400745;
-    }
-    div.stButton > button:first-child {
-        background-color: #ffffff;
-        color:#000000;
-    }
-    .css-z09lfk:focus:not(:active) {
-        border-color: #ffffff;
-        box-shadow: none;
-        background-color: #ffffff;
-        color:#000000;
-    }
-    .css-z09lfk:focus:(:active) {
-        border-color: #ffffff;
-        box-shadow: #832196;
-        color: #ffffff;
-        background-color: #000000;
-    }
-    .css-z09lfk:focus:(:active){
-        background-color: #000000;
-        border-color: #ffffff;
-        box-shadow: #832196;
-        color: #ffffff;
-        background-color: #000000;
-    }
-
-</style>
-""", unsafe_allow_html=True)
 
 def RunAI(string, classifier):
     sentences = sent_tokenize(string)
@@ -54,52 +22,79 @@ def RunAI(string, classifier):
         if lr_predict(sentence) == 1:
           flags.append(sentence)
       return flags
+
+    elif classifier == "XGBSuicide":
+      for sentence in sentences:
+        if xgb_suicide(sentence) == 1:
+          flags.append(sentence)
+      return flags
+    
+    elif classifier == "LogisticSuicide":
+      for sentence in sentences:
+        if lr_suicide(sentence) == 1:
+          flags.append(sentence)
+      return flags
     
 
-def Display(essay, col1, col2, col3):
-    if essay != "":
-        st.info("Loading...")
-        xgflags = RunAI(essay, "XGBoost")
-        lrflags = RunAI(essay, "LogisticRegression")
-        count = 0
-        
-        with col1:
-            for flag in lrflags:
+def ShowFlags(flaglist):
+    count = 0
+    flagcount = len(flaglist)
+    if flagcount == 0:
+        st.write("No red flags found!")
+    else:
+        with st.expander("Found "+str(flagcount)+" red flag/s:", True):
+            for flag in flaglist:
                 count+=1
                 st.write(str(count)+". "+flag)
-            if count == 0:
-                st.write("No red flags found!")
+            st.write("\n")
 
-        count = 0
+def Display(essay):
+    if essay != "":
+        st.info("Loading...")
+        
+        xgflags = RunAI(essay, "XGBoost")
+        lrflags = RunAI(essay, "LogisticRegression")
+        xgsflags = RunAI(essay, "XGBSuicide")
+        lrsflags = RunAI(essay, "LogisticSuicide")
+        
+        with col1:
+            ShowFlags(lrflags)
         
         with col2:
-            for flag in xgflags:
-                count += 1
-                st.write(str(count)+". "+flag)
-            if count == 0:
-                st.write("No red flags found!")
+            ShowFlags(xgflags)
+        
+        with col3:
+            ShowFlags(lrsflags)
+        
+        with col4:
+            ShowFlags(xgsflags)
 
         st.success("Calculations complete!")
 
-st.title(":red[under construction lol]")
-st.title("Welcome to :violet[T.E.D.D.Y]")
-st.subheader("Text-based Early Depression Detector for Youth", anchor="welcome-to-t-e-d-d-y")
-st.text("Enter your essay into the sidebar.")
-st.text("")
+st.title("Welcome to :violet[T.E.D.D.Y.]")
+st.subheader("Text-based Early Distress Detector for Youth", anchor="welcome-to-t-e-d-d-y")
+st.write("Enter your essay into the sidebar.")
+st.write("\n")
 
-col1, col2, col3 = st.columns(3)
-with col1:
-    col1h = st.header("High-Sensitivity Test")
-    st.caption("_with Logistic Regression_")
+col4, col3 = st.columns(2)
 
-with col2:
-    col2h = st.header("Low-Sensitivity Test")
-    st.caption("_with XGBoost_")
+with col4:
+    st.subheader("Extremely High-Risk Test")
 
 with col3:
-    st.header("Grammatical Test")
+    st.subheader("High-Risk Test")
+
+col2, col1 = st.columns(2)
+
+with col2:
+    col2h = st.subheader("Medium-Risk Test")
+    
+with col1:
+    col1h = st.subheader("Low-Risk Test")
 
 with st.sidebar:
     st.title("Copy-paste an essay into the box below!")
     essay = st.text_input("Enter text here...")
-    st.button("Submit", on_click=Display(essay, col1, col2, col3))
+    submit = st.button("Submit")
+    if submit:
+        headers = Display(essay)
