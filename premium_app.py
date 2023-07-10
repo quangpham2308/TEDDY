9d74396a4c2696905e66321962c749c576a5f0fa
Aleystre Vocleine <40627577+AleystreV@users.noreply.github.com>
2023-07-10 14:56:15 +0800

    Update premium_app.py

--- premium_app.py
+++ premium_app.py
@@ -127,7 +127,7 @@ def Refresh():
     st.stop()
 
 
-st.title("Welcome to :violet[T.E.D.D.Y. Premium]")
+st.title("Welcome to :violet[T.E.D.D.Y. Bulk]")
 st.subheader("Text-based Early Distress Detector for Youth", anchor="welcome-to-t-e-d-d-y")
 st.caption("In the sidebar, enter any sufficient amount of text* that is reflective of a person's thoughts: essays, reflections, and chat conversations work best. T.E.D.D.Y. will use artificial intelligence to display sentences that may be a cause for concern.\n :red[T.E.D.D.Y. is not meant to be used as a diagnostic tool] - it is designed to give you a general idea of whether someone in your school or workplace might need more emotional support.")
 st.caption("*:violet[A note to our testers:] Our research shows that the sentiments expressed in actual student data are subtle. :red[Thus, arbitrary or made-up statements for testing do not yield good results.] If you do not have actual student data for testing, we recommend searching for sample student essays (e.g., blog posts, college admission essay samples, etc.) online.")
