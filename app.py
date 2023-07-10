eca9ec6ff0cbcdc6c89766e846da8c57d1760667
Aleystre Vocleine <40627577+AleystreV@users.noreply.github.com>
2023-07-10 12:16:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -137,6 +137,6 @@ with st.sidebar:
     essay = st.text_input("Copy-paste text here:")
     submit = st.button("Submit Text", on_click=Display(essay))
     uploaded_file = st.file_uploader('Or, upload a PDF file:', type="pdf")
-    submit2 = st.button("Submit Files", on_click=Display(GetPDFText(uploaded_file)))
+    submit2 = st.button("Submit File", on_click=Display(GetPDFText(uploaded_file)))
     st.write("[Learn more about T.E.D.D.Y!](https://hh.aanyagupta15.repl.co/index.html)")
     st.write("[Help us improve!](https://forms.gle/eAYpKmd9udkdFUir6)")
